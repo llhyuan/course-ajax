4ef07d872ca8e8f2a7d6f4b3e516e605a2ad1756
llhyuan <lhyuan.liu21@icloud.com>
2023-01-15 15:25:14 +0100

    Update app.js

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -1,12 +1,45 @@
 (function () {
-    const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+  /* Select all the elements that we need */
+  const form = document.querySelector("#search-form");
+  const searchField = document.querySelector("#search-keyword");
+  let searchedForText;
+  const responseContainer = document.querySelector("#response-container");
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
-    });
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    responseContainer.innerHTML = "";
+    searchedForText = searchField.value;
+
+    const unsplashRequest = new XMLHttpRequest();
+    unsplashRequest.open(
+      "GET",
+      `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`
+    );
+
+    unsplashRequest.setRequestHeader(
+      "authorization",
+      "Client-ID FDKJEQ79W9ljmSNMBCHH3ln4WRdKhr06kxkfYlN0BPc"
+    );
+
+    unsplashRequest.onload = addImage;
+
+    unsplashRequest.send();
+  });
+
+  function addImage() {
+    let htmlContent = "";
+    const data = JSON.parse(this.responseText);
+    const firstImage = data.results[0];
+
+    if (data && data.results && firstImage) {
+      htmlContent = `<figure>
+                    <img src="${firstImage.urls.full}" alt="${searchedForText}" load="lazy"/>
+                    <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                  </figure>`;
+
+      responseContainer.insertAdjacentHTML("beforebegin", htmlContent);
+    } else {
+      htmlContent = `<div class="error-no-image">No image available</div>`;
+    }
+  }
 })();
