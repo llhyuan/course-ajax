5b3f8a2ce81ee254a47442df472665ca502ba110
llhyuan <lhyuan.liu21@icloud.com>
2023-01-20 08:16:47 +0100

    working version of all three async requests.

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -5,11 +5,14 @@
   let searchedForText;
   const responseContainer = document.querySelector("#response-container");
 
-  form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", requestHandler);
+
+  function requestHandler(e) {
     e.preventDefault();
     responseContainer.innerHTML = "";
     searchedForText = searchField.value;
 
+    /* Setting up image request */
     const unsplashRequest = new XMLHttpRequest();
     unsplashRequest.open(
       "GET",
@@ -23,8 +26,19 @@
 
     unsplashRequest.onload = addImage;
 
+    /* Setting up article request */
+    const nytHttpRequest = new XMLHttpRequest();
+    nytHttpRequest.open(
+      "GET",
+      `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=25UAPkIu3przpAwJsySAKfPwL5VUX2IK`
+    );
+
+    nytHttpRequest.onload = addArticle;
+
+    /* Send out the requests */
     unsplashRequest.send();
-  });
+    nytHttpRequest.send();
+  }
 
   function addImage() {
     let htmlContent = "";
@@ -36,10 +50,34 @@
                     <img src="${firstImage.urls.full}" alt="${searchedForText}" load="lazy"/>
                     <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
                   </figure>`;
-
-      responseContainer.insertAdjacentHTML("beforebegin", htmlContent);
     } else {
       htmlContent = `<div class="error-no-image">No image available</div>`;
     }
+
+    responseContainer.insertAdjacentHTML("beforebegin", htmlContent);
+  }
+
+  function addArticle() {
+    let htmlContent = "";
+    const data = JSON.parse(this.responseText);
+    console.log(data.response.docs);
+
+    if (data && data.response && data.response.docs) {
+      htmlContent =
+        "<ul>" +
+        data.response.docs
+          .map(function (article) {
+            return `<li class="article">
+          <h2><a href="${article.web_url}" target="_blank">${article.headline.main}</a></h2>
+          <p>${article.snippet}</p>
+      </li>`;
+          })
+          .join("") +
+        "</ul>";
+    } else {
+      htmlContent = `<div class="error-no-image">No image available</div>`;
+    }
+
+    responseContainer.insertAdjacentHTML("beforeend", htmlContent);
   }
 })();
--- lesson-2-async-w-jQuery/app.js
+++ lesson-2-async-w-jQuery/app.js
@@ -1,14 +1,69 @@
 /* eslint-env jquery */
 
 (function () {
-    const form = document.querySelector('#search-form');
-    const searchField = document.querySelector('#search-keyword');
-    let searchedForText;
-    const responseContainer = document.querySelector('#response-container');
+  /* Select all the elements that we need */
+  const form = document.querySelector("#search-form");
+  const searchField = document.querySelector("#search-keyword");
+  let searchedForText;
+  const responseContainer = document.querySelector("#response-container");
 
-    form.addEventListener('submit', function (e) {
-        e.preventDefault();
-        responseContainer.innerHTML = '';
-        searchedForText = searchField.value;
-    });
+  form.addEventListener("submit", requestHandler);
+
+  function requestHandler(e) {
+    e.preventDefault();
+    responseContainer.innerHTML = "";
+    searchedForText = searchField.value;
+
+    /* Setting up image request */
+    $.ajax({
+      url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+      headers: {
+        authorization: "Client-ID FDKJEQ79W9ljmSNMBCHH3ln4WRdKhr06kxkfYlN0BPc",
+      },
+    }).done(addImage);
+
+    /* Setting up article request */
+    $.ajax({
+      url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=25UAPkIu3przpAwJsySAKfPwL5VUX2IK`,
+    }).done(addArticle);
+  }
+
+  function addImage(images) {
+    let htmlContent = "";
+    const firstImage = images.results[0];
+
+    if (images && images.results && firstImage) {
+      htmlContent = `<figure>
+                      <img src="${firstImage.urls.full}" alt="${searchedForText}" load="lazy"/>
+                      <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+                    </figure>`;
+    } else {
+      htmlContent = `<div class="error-no-image">No image available</div>`;
+    }
+
+    responseContainer.innerHTML = "";
+    responseContainer.insertAdjacentHTML("afterbegin", htmlContent);
+  }
+
+  function addArticle(articles) {
+    let htmlContent = "";
+
+    if (articles && articles.response && articles.response.docs) {
+      htmlContent =
+        "<ul>" +
+        articles.response.docs
+          .map(function (article) {
+            return `<li class="article">
+            <h2><a href="${article.web_url}" target="_blank">${article.headline.main}</a></h2>
+            <p>${article.snippet}</p>
+        </li>`;
+          })
+          .join("") +
+        "</ul>";
+    } else {
+      htmlContent = `<div class="error-no-image">No image available</div>`;
+    }
+
+    responseContainer.insertAdjacentHTML("beforeend", htmlContent);
+  }
 })();
